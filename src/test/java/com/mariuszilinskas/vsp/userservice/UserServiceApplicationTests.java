35a14b080760a9c9282652f1afd99c84d99e9f7e
Marius Zilinskas <mariuzmail@gmail.com>
2024-04-24 00:19:23 +0100

    user service initiated

---
+++ src/test/java/com/mariuszilinskas/vsp/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariuszilinskas.vsp.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
