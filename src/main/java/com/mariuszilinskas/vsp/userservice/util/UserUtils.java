b945da73d207badc55594e8a83aae977c10dfd39
Marius Zilinskas <mariuzmail@gmail.com>
2024-04-30 21:55:45 +0100

    created user utils class

---
+++ src/main/java/com/mariuszilinskas/vsp/userservice/util/UserUtils.java
@@ -0,0 +1,12 @@
+package com.mariuszilinskas.vsp.userservice.util;
+
+public abstract class UserUtils {
+
+    private UserUtils() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd hh:mm:ss";
+
+    public static final long PIN_CODE_VALID_MINUTES = 15;
+}
