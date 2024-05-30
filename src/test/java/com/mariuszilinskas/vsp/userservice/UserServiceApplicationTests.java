6b8b4d009929e87850ec2c8ae38384a68a6d7575
Marius Zilinskas <mariuzmail@gmail.com>
2024-05-30 14:55:45 +0100

    updated UserServiceApplicationTests

--- src/test/java/com/mariuszilinskas/vsp/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/mariuszilinskas/vsp/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,46 @@
 package com.mariuszilinskas.vsp.userservice;
 
+import com.mariuszilinskas.vsp.userservice.client.AuthFeignClient;
+import com.mariuszilinskas.vsp.userservice.repository.UserRepository;
+import com.mariuszilinskas.vsp.userservice.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Test class for the Spring application context and bean configuration in the UserService application.
+ */
 @SpringBootTest
 class UserServiceApplicationTests {
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AuthFeignClient authFeignClient;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void userServiceBeanLoads() {
+        assertNotNull(userService, "User Service should have been auto-wired by Spring Context");
+    }
+
+    @Test
+    void userRepositoryBeanLoads() {
+        assertNotNull(userRepository, "User Repository should have been auto-wired by Spring Context");
+    }
+
+    @Test
+    void authFeignClientBeanLoads() {
+        assertNotNull(authFeignClient, "Auth Feign Client should have been auto-wired by Spring Context");
     }
 
 }
