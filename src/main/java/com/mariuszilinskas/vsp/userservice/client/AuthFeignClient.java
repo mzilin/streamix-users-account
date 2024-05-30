7c12174b813ad2e07bb351aac4f5894a1c29d960
Marius Zilinskas <mariuzmail@gmail.com>
2024-05-30 12:53:00 +0100

    added password/verify request to  auth feign client

--- src/main/java/com/mariuszilinskas/vsp/userservice/client/AuthFeignClient.java
+++ src/main/java/com/mariuszilinskas/vsp/userservice/client/AuthFeignClient.java
@@ -1,6 +1,6 @@
 package com.mariuszilinskas.vsp.userservice.client;
 
-import com.mariuszilinskas.vsp.userservice.dto.CreateCredentialsRequest;
+import com.mariuszilinskas.vsp.userservice.dto.CredentialsRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface AuthFeignClient {
 
     @PostMapping(value = "/credentials", consumes = "application/json")
-    ResponseEntity<Void> createPasswordAndSetPasscode(@RequestBody CreateCredentialsRequest request);
+    ResponseEntity<Void> createPasswordAndSetPasscode(@RequestBody CredentialsRequest request);
+
+    @PostMapping(value = "/password/verify", consumes = "application/json")
+    ResponseEntity<Void> verifyPassword(@RequestBody CredentialsRequest request);
 
 }
