28fef5e212a9b23c451196c1e54febc8d1e2d0c9
Marius Zilinskas <mariuzmail@gmail.com>
2024-06-21 23:23:13 +0100

    update RabbitMQProducer with more routing keys and methods

--- src/main/java/com/mariuszilinskas/vsp/userservice/producer/RabbitMQProducer.java
+++ src/main/java/com/mariuszilinskas/vsp/userservice/producer/RabbitMQProducer.java
@@ -1,12 +1,14 @@
 package com.mariuszilinskas.vsp.userservice.producer;
 
-import com.mariuszilinskas.vsp.userservice.dto.CreateDefaultUserProfileRequest;
+import com.mariuszilinskas.vsp.userservice.dto.CreateUserDefaultProfileRequest;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -21,9 +23,25 @@ public class RabbitMQProducer {
     @Value("${rabbitmq.routing-keys.profile-setup}")
     private String profileSetupRoutingKey;
 
-    public void sendCreateDefaultUserProfileMessage(CreateDefaultUserProfileRequest request) {
+    @Value("${rabbitmq.routing-keys.reset-passcode}")
+    private String createPasscodeRoutingKey;
+
+    @Value("${rabbitmq.routing-keys.delete-user-data}")
+    private String deleteUserDataRoutingKey;
+
+    public void sendCreateUserDefaultProfileMessage(CreateUserDefaultProfileRequest request) {
         logger.info("Sending message to create default user profile: {}", request);
         rabbitTemplate.convertAndSend(exchange, profileSetupRoutingKey, request);
+    }
+
+    public void sendResetPasscodeMessage(UUID userId) {
+        logger.info("Sending message to create user passcode: {}", userId);
+        rabbitTemplate.convertAndSend(exchange, createPasscodeRoutingKey, userId);
+    }
+
+    public void sendDeleteUserDataMessage(UUID userId) {
+        logger.info("Sending message to delete user data for User [id: {}]", userId);
+        rabbitTemplate.convertAndSend(exchange, deleteUserDataRoutingKey, userId);
     }
 
 }
