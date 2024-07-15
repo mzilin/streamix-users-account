4008ae1d713ca3a70b8d4424ca0bcd51879cf68c
Marius Zilinskas <mariuzmail@gmail.com>
2024-07-15 22:02:19 +0100

    update AuthFeignClient

--- src/main/java/com/mariuszilinskas/vsp/userservice/client/AuthFeignClient.java
+++ src/main/java/com/mariuszilinskas/vsp/userservice/client/AuthFeignClient.java
@@ -5,6 +5,7 @@ import com.mariuszilinskas.vsp.userservice.dto.VerifyPasswordRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient("auth")
@@ -13,7 +14,7 @@ public interface AuthFeignClient {
     @PostMapping(value = "/credentials", consumes = "application/json")
     ResponseEntity<Void> createPasswordAndSetPasscode(@RequestBody CredentialsRequest request);
 
-    @PostMapping(value = "/password/verify", consumes = "application/json")
+    @PutMapping(value = "/password/verify", consumes = "application/json")
     ResponseEntity<Void> verifyPassword(@RequestBody VerifyPasswordRequest request);
 
 }
