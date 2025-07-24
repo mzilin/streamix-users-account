804f15769e7e24ff90d55263c6f7a6f2bf3d5534
Marius Zilinskas <mariuzmail@gmail.com>
2025-07-24 19:59:47 +0100

    add UserAdminMapper tests

--- src/test/java/com/mariuszilinskas/vsp/users/account/mapper/UserAdminMapperTest.java
+++ src/test/java/com/mariuszilinskas/vsp/users/account/mapper/UserAdminMapperTest.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserAdminMapperTest {
 
