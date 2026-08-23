68bbcc853d34428a5ef0595acb375e79b1c0c8f9
Marius Zilinskas <mariuzmail@gmail.com>
2026-08-23 19:52:38 +0100

    add streamix-web-commons package

--- src/main/java/com/mariuszilinskas/streamix/users/account/handler/AppExceptionHandler.java
+++ src/main/java/com/mariuszilinskas/streamix/users/account/handler/AppExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.mariuszilinskas.streamix.users.account.handler;
 
 
-import com.mariuszilinskas.streamix.users.account.dto.ErrorResponse;
-import com.mariuszilinskas.streamix.users.account.dto.FieldErrorResponse;
+import com.mariuszilinskas.streamix.web.dto.ErrorResponse;
+import com.mariuszilinskas.streamix.web.dto.FieldErrorResponse;
 import com.mariuszilinskas.streamix.users.account.exception.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
